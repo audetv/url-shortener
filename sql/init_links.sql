b3a4f24a90362eae438d3e67655f0b2414919255
agusev <audetv@gmail.com>
2023-05-06 14:25:31 +0300

    Изменена схема links, добавлен первичный ключ

--- sql/init_links.sql
+++ sql/init_links.sql
@@ -4,5 +4,6 @@ CREATE TABLE public.links (
 	redirect_count int,
 	created_at timestamptz NOT NULL,
 	updated_at timestamptz NOT NULL,
-	deleted_at timestamptz NULL
-)
+	deleted_at timestamptz NULL,
+    CONSTRAINT links_pk PRIMARY KEY (short)
+);
