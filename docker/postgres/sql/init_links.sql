b127d0a6f59de042f3e68892af336581534f59cc
agusev <audetv@gmail.com>
2023-05-07 15:15:49 +0300

    Изменено поле url на text

--- docker/postgres/sql/init_links.sql
+++ docker/postgres/sql/init_links.sql
@@ -1,6 +1,6 @@
 CREATE TABLE public.links (
 	short varchar NOT NULL,
-	url varchar(512) NOT NULL,
+	url text NOT NULL,
 	redirect_count int,
 	created_at timestamptz NOT NULL,
 	updated_at timestamptz NOT NULL,
