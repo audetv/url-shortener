445d4be1c0c381999f083fe7cbf08d91e16c96f7
agusev <audetv@gmail.com>
2023-05-08 17:29:59 +0300

    Добавлена функция processLink. Подготавливает ссылку перед сохранением

---
+++ docker/postgres/sql/update_links.sql
@@ -0,0 +1,13 @@
+UPDATE links SET url='https://svodd.ru/?search[query]=жешув&search[matching]=&search[matching]=query_string&search[date]=&search[dictionary]=0' WHERE short='gUJnXIs8';
+
+UPDATE links SET url='https://svodd.ru/?search[query]=мемедович&search[matching]=&search[matching]=match_phrase&search[date]=&search[dictionary]=0' WHERE short='wtQ1VycF';
+
+UPDATE links SET url='https://svodd.ru/?search[query]= фурии изображались с факелами из тисовых веток. элевсинские жрецы украшали себя венками из миртовых и тисовых веток. в древнерусской литературе упоминание о тисовом дереве можно встретить в слове о полку игореве&search[matching]=&search[matching]=query_string' WHERE short='NIt3vXuV';
+
+UPDATE links SET url='http://svodd.ru/?search[query]=депрессия гнев торговля кюблер-росс&search[matching]=&search[matching]=query_string&search[date]=&search[dictionary]=0' WHERE short='khdI8wEa';
+
+UPDATE links SET url='http://svodd.ru/?search[query]=инглинги&search[matching]=&search[matching]=query_string&search[date]=&search[dictionary]=0' WHERE short='cFccmKYX';
+
+UPDATE links SET url='http://svodd.ru/?search[query]=материя&search[matching]=&search[matching]=query_string&search[date]=&search[dictionary]=0' WHERE short='p8OPytsL';
+
+UPDATE links SET url='http://svodd.ru/?search[query]=сводд&search[matching]=&search[matching]=query_string&search[date]=&search[dictionary]=0' WHERE short='mh9YvKQD';
