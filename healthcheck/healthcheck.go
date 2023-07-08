b9e4e2b6c13becf98314cc0a92d71abc5c5a625e
agusev <audetv@gmail.com>
2023-07-09 00:12:14 +0300

    Fixed resp body close

--- healthcheck/healthcheck.go
+++ healthcheck/healthcheck.go
@@ -7,8 +7,9 @@ import (
 )
 
 func main() {
-	_, err := http.Get(fmt.Sprintf("http://127.0.0.1:%s/health", os.Getenv("PORT")))
+	resp, err := http.Get(fmt.Sprintf("http://127.0.0.1:%s/health", os.Getenv("PORT")))
 	if err != nil {
 		os.Exit(1)
 	}
+	defer resp.Body.Close()
 }
