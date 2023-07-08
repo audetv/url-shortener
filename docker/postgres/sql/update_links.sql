d6322362b422404242f954cc5a8f7f6d92f5899e
agusev <audetv@gmail.com>
2023-07-08 22:27:23 +0300

    updated update_links.sql

--- docker/postgres/sql/update_links.sql
+++ docker/postgres/sql/update_links.sql
@@ -1,16 +1,16 @@
-UPDATE links SET url='https://svodd.ru/?search[query]=жешув&search[matching]=&search[matching]=query_string&search[date]=&search[dictionary]=0' WHERE short='gUJnXIs8';
+UPDATE links SET url='https://svodd.ru/?search[query]=Жешув&search[matching]=&search[matching]=query_string&search[date]=&search[dictionary]=0' WHERE short='gUJnXIs8';
 
-UPDATE links SET url='https://svodd.ru/?search[query]=мемедович&search[matching]=&search[matching]=match_phrase&search[date]=&search[dictionary]=0' WHERE short='wtQ1VycF';
+UPDATE links SET url='https://svodd.ru/?search[query]=Мемедович&search[matching]=&search[matching]=match_phrase&search[date]=&search[dictionary]=0' WHERE short='wtQ1VycF';
 
 UPDATE links SET url='https://svodd.ru/?search[query]= фурии изображались с факелами из тисовых веток. элевсинские жрецы украшали себя венками из миртовых и тисовых веток. в древнерусской литературе упоминание о тисовом дереве можно встретить в слове о полку игореве&search[matching]=&search[matching]=query_string' WHERE short='NIt3vXuV';
 
-UPDATE links SET url='http://svodd.ru/?search[query]=депрессия гнев торговля кюблер-росс&search[matching]=&search[matching]=query_string&search[date]=&search[dictionary]=0' WHERE short='khdI8wEa';
+UPDATE links SET url='http://svodd.ru/?search[query]=Депрессия гнев торговля кюблер-росс&search[matching]=&search[matching]=query_string&search[date]=&search[dictionary]=0' WHERE short='khdI8wEa';
 
-UPDATE links SET url='http://svodd.ru/?search[query]=инглинги&search[matching]=&search[matching]=query_string&search[date]=&search[dictionary]=0' WHERE short='cFccmKYX';
+UPDATE links SET url='http://svodd.ru/?search[query]=Инглинги&search[matching]=&search[matching]=query_string&search[date]=&search[dictionary]=0' WHERE short='cFccmKYX';
 
 UPDATE links SET url='http://svodd.ru/?search[query]=материя&search[matching]=&search[matching]=query_string&search[date]=&search[dictionary]=0' WHERE short='p8OPytsL';
 
-UPDATE links SET url='http://svodd.ru/?search[query]=сводд&search[matching]=&search[matching]=query_string&search[date]=&search[dictionary]=0' WHERE short='mh9YvKQD';
+UPDATE links SET url='http://svodd.ru/?search[query]=СВОДД&search[matching]=&search[matching]=query_string&search[date]=&search[dictionary]=0' WHERE short='mh9YvKQD';
 
 ALTER TABLE public.links ADD "search" text NULL;
 
@@ -70,3 +70,7 @@ UPDATE links SET search='цой' WHERE short='135CETWj';
 
 UPDATE links SET search='' WHERE short='1d4tdQ0u';
 
+
+UPDATE links SET search='шеннон колмогоров' WHERE short='63bABW9x';
+
+UPDATE links SET search='' WHERE short='ZA8F5CIN';
